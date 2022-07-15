ae6842b70ba1dbfc280d240aa8fcbdb22604db62
Seb <sebastien.lucas@epitech.eu>
2022-07-16 01:18:44 +0400

    Ignore non .pdf files to avoid errors

--- main.py
+++ main.py
@@ -12,9 +12,17 @@ final_Name = str(input()) + ".pdf"
 if __name__ == '__main__':
     print(path)
     for dirpath, dirnames, filenames in walk(path):
-        files.extend(filenames)
+        for file in filenames:
+            if ".pdf" in file:
+                files.append(file)
+            else:
+                pass
     print(files)
     for pdf_file in files:
         merger.append("pdf_files/"+pdf_file)
     merger.write(final_Name)
     merger.close()
+    print()
+    print("Done!")
+    print()
+    print(f"File named '{final_Name}' has been saved at '{os.getcwd()}'")
