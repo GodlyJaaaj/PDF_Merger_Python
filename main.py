4b2a81ec20225c578fec5fe0b30d6cc2e712b2b5
Seb <sebastien.lucas@epitech.eu>
2022-07-16 23:54:20 +0400

    Added
    - a check to not overwrite already existing file
    - a check if no pdf files are provided
    
    also simplified a little bit the code

--- main.py
+++ main.py
@@ -2,27 +2,38 @@ from PyPDF2 import PdfFileMerger
 import os
 from os import walk
 
-
 merger = PdfFileMerger()
 path = os.getcwd() + r"\pdf_files"
 files = []
-print("Enter final pdf file name (only name) : ")
-final_Name = str(input()) + ".pdf"
+final_Name = ""
+while not any(final_Name):
+    print("Enter final pdf file name (only name) : ")
+    final_Name = str(input()) + ".pdf"
+    if final_Name in os.listdir(path.rstrip(r"\pdf_files")):
+        print(f"File '{final_Name}' already exists")
+        print()
+        final_Name = ""
+    else:
+        break
 
 if __name__ == '__main__':
     print(path)
-    for dirpath, dirnames, filenames in walk(path):
-        for file in filenames:
-            if ".pdf" in file:
-                files.append(file)
-            else:
-                pass
+    for file in os.listdir(path):
+        if file.endswith(".pdf"):
+            files.append(file)
+        else:
+            pass
+    print(f"{len(files)} pdf files detected")
     print(files)
-    for pdf_file in files:
-        merger.append("pdf_files/"+pdf_file)
-    merger.write(final_Name)
-    merger.close()
+    if not files:
+        print("No pdf files found in the directory")
+    else:
+        for pdf_file in files:
+            merger.append("pdf_files/" + pdf_file)
+        merger.write(final_Name)
+        merger.close()
+        print()
+        print("Done!")
+        print()
+        print(f"File named '{final_Name}' has been saved at '{os.getcwd()}'")
     print()
-    print("Done!")
-    print()
-    print(f"File named '{final_Name}' has been saved at '{os.getcwd()}'")
